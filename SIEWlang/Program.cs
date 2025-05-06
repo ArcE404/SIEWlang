467b3424e3ec3925bbca257eebeca15231a3fd1b
Augusto <82833443+ArcE404@users.noreply.github.com>
2025-05-05 21:56:34 -0600

    Adding basic structure and core folder

---
+++ SIEWlang/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SIEWlang;
+
+// ***************************************************
+// Sometimes It Even Works (SIEW) Programming Language
+// ***************************************************
+class Program
+{
+    static void Main(string[] args)
+    {
+        Siew siewProgram = new();
+
+        siewProgram.Start(args);
+    }
+}
