6eb19278927711995996757ddaef9bfe6ea2595a
Augusto <82833443+ArcE404@users.noreply.github.com>
2025-06-29 16:36:39 -0600

    re order the code

--- SIEWlang/Core/Interpreter/Environment.cs
+++ SIEWlang/Core/Interpreter/Environment.cs
@@ -1,13 +1,16 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Xml.Schema;
-using SIEWlang.Core.Lexer;
+﻿using SIEWlang.Core.Lexer;
 
 namespace SIEWlang.Core.Interpreter;
 
 public class Environment
 {
-    
     private readonly Environment? enclosing;
+
+    // Even though a variable is represented by a Token, we use the variable name (string) as the key instead of the Token itself.
+    // This is because the IDENTIFIER Token corresponds to a specific location in the source code.
+    // However, when resolving variables, all IDENTIFIER tokens with the same name should refer to the same logical variable,
+    // regardless of where they appear in the code. Therefore, we store and compare variable names (strings), not Token instances.
+    private readonly Dictionary<string, object?> values = new();
 
     public Environment()
     {
@@ -19,42 +22,9 @@ public class Environment
         enclosing = env;
     }
 
-    // Even though a variable is represented by a Token, we use the variable name (string) as the key instead of the Token itself.
-    // This is because the IDENTIFIER Token corresponds to a specific location in the source code.
-    // However, when resolving variables, all IDENTIFIER tokens with the same name should refer to the same logical variable,
-    // regardless of where they appear in the code. Therefore, we store and compare variable names (strings), not Token instances.
-    private readonly Dictionary<string, object?> values = new();
-
-    public void Define(string name, object? value)
-    {
-        values[name] = value;
-        /*
-         * This is a deliberate semantic choice:
-         * We allow variables to be redefined by declaring them again with "var". For example:
-         * 
-         * var a = "some";
-         * print a; // prints: some
-         * var a = "some some";
-         * print a; // prints: some some
-         * 
-         * This behavior is permitted in Scheme at the top level, and we adopt the same convention here.
-         * 
-         * "When in doubt, do what Scheme does." 
-         */
-    }
-
-    public object? Get(Token name)
-    {
-        if  (values.TryGetValue(name.Lexeme, out var value)) return value;
-
-        if (enclosing is not null) return enclosing.Get(name);
-
-        throw new RuntimeError(name, $"RunTimeError: Undefined variable {name.Lexeme}.");
-    }
-
     public void Assing(Token name, object value)
     {
-        if(values.ContainsKey(name.Lexeme))
+        if (values.ContainsKey(name.Lexeme))
         {
             values[name.Lexeme] = value;
             return;
@@ -67,5 +37,32 @@ public class Environment
         }
 
         throw new RuntimeError(name, $"RunTimeError: Undefined variable '{name.Lexeme}'");
+    }
+
+    public void Define(string name, object? value)
+    {
+        values[name] = value;
+        /*
+         * This is a deliberate semantic choice:
+         * We allow variables to be redefined by declaring them again with "var". For example:
+         *
+         * var a = "some";
+         * print a; // prints: some
+         * var a = "some some";
+         * print a; // prints: some some
+         *
+         * This behavior is permitted in Scheme at the top level, and we adopt the same convention here.
+         *
+         * "When in doubt, do what Scheme does."
+         */
+    }
+
+    public object? Get(Token name)
+    {
+        if (values.TryGetValue(name.Lexeme, out var value)) return value;
+
+        if (enclosing is not null) return enclosing.Get(name);
+
+        throw new RuntimeError(name, $"RunTimeError: Undefined variable {name.Lexeme}.");
     }
 }
